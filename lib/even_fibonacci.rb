6dd31c67d86e314b68563b8923577489e01eddee
yoshikot <ytamaoki@gmail.com>
2015-11-22 17:55:52 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(number)
+  collect_even(number).reduce(:+) 
+end
+
+def collect_even(number)
+  fib = [1,2]
+  while fib[fib.length - 2] + fib[fib.length - 1] < number
+    fib << fib[fib.length - 2] + fib[fib.length - 1]
+  end
+  fib.select {|figure| figure % 2 == 0}
+end
+
+print "#{even_fibonacci_sum(4000000)}"
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(number)
+    @number = number
+  end
+
+  def sum
+    collect_even.reduce(:+) 
+  end
+
+  def collect_even
+    fib = [1,2]
+    while fib[fib.length - 2] + fib[fib.length - 1] < @number
+      fib << fib[fib.length - 2] + fib[fib.length - 1]
+    end
+    fib.select {|figure| figure % 2 == 0}
+  end
+
+end
